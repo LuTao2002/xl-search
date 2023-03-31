48a28a2c8c074198c241fb5ba91825eb4f3d7d6b
LT <2959473720@qq.com>
2023-03-31 16:32:35 +0800

    修改router

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import store from './store'
 import './plugins/element.js'
 import axios from "axios"
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
